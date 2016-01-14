3b6f33a024ae475e48d6af0739641b6441b47fc0
hazelriggc <chazelrigg@barkleyus.com>
2016-01-14 13:55:27 -0600

    linking text to twilio with response

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ var bodyParser = require('body-parser');
 console.log('server running on port 3000');
 app.use(bodyParser.urlencoded({ extended: true }));
 server.listen('3000');
+
+app.post('/new-text-message', function(req, res){
+	var userInput = req.body.Body.toLowerCase();
+	var message;
+
+	if(userInput.indexOf("iot")!= -1){
+		message = "You signed up for Internet of Things!";
+	}
+	if(userInput.indexOf("make")!= -1){
+		message = "You signed up for Make!";
+	}
+	if(userInput.indexOf("3d")!= -1 || userInput.indexOf("3-d")!= -1){
+		message = "You signed up for 3-D printing!";
+	}
+	res.setHeader("Content-Type", "text/xml");
+	res.send('<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Message>' + message + '</Message></Response>');
+});
+
