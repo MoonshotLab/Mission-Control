ac5479a0686e78a4de699489e209594f65af1b3f
Joe Longstreet <joelongstreet@gmail.com>
2016-02-09 16:44:26 -0600

    can now query by date

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 $(function(){
+  // query from up to one month ago
+  var startDate = new Date().getTime()-2592000000;
+
   $.ajax({
-    url : '/gratitudes'
+    url : '/gratitudes?startDate=' + startDate
   }).done(function(gratitudes){
 
     var template = _.template(
@@ -10,6 +13,6 @@ $(function(){
     gratitudes.forEach(function(gratitude){
       $('body').append(template(gratitude));
     });
-
   });
+
 });
