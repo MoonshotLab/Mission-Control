b2232bbd3be3f21410400622281de118152a47a9
Joe Longstreet <joelongstreet@gmail.com>
2016-04-07 18:44:55 -0500

    dont block the ui when navigating between slide types

--- public/tvos.js
+++ public/tvos.js
@@ -44,15 +44,20 @@ var socketEvent = function(data){
 var startSlideshow = function(){
   if(slides.playbackInterval) clearInterval(slides.playbackInterval);
 
+  // add a loading slide
+  slides.lastPlayed = makeLoadingDocument();
+  navigationDocument.pushDocument(slides.lastPlayed);
+
   var url = config.ROOT_URL + '/api/' + config.playbackType;
   fetchCollection(url, function(collection){
+    // save the collection and apply the first slide
     slides.collection = collection;
-
     if(slides.collection[slides.currentIndex]){
       var slide = slides.collection[slides.currentIndex];
       changeSlide({ type : slide.type, id : slide._id });
     }
 
+    // setup a plaback interval
     slides.playbackInterval = setInterval(function(){
       slides.currentIndex++;
       if(!slides.collection[slides.currentIndex])
@@ -87,6 +92,7 @@ var changeSlide = function(opts){
       var doc = request.responseXML;
       navigationDocument.pushDocument(doc);
 
+      // listen for selections from the carousel
       doc.getElementById('carousel').addEventListener('select', function(e){
         var type = e.target.getAttribute('type');
         config.playbackType = type;
@@ -99,22 +105,12 @@ var changeSlide = function(opts){
       var nextSlide = request.responseXML;
       navigationDocument.pushDocument(nextSlide);
 
-      // the only way to listen for a back button press is the unload method
+      // listen for a back button press
       nextSlide.addEventListener('unload', backButtonHandler);
 
-      if(slides.lastPlayed){
-        setTimeout(function(){
-          // don't accidentally call the unload method
-          slides.lastPlayed.removeEventListener('unload', backButtonHandler);
-
-          // pressing the back button can sometimes mess us all up
-          try{
-            navigationDocument.removeDocument(slides.lastPlayed);
-          } catch(e){}
-
-          slides.lastPlayed = nextSlide;
-        }, 1000);
-      } else slides.lastPlayed = nextSlide;
+      // schedule a removal of the last slide or the loader
+      if(slides.lastPlayed) scheduleSlideRemoval(nextSlide);
+      else slides.lastPlayed = nextSlide;
     }
   }, false);
 
@@ -133,6 +129,42 @@ var fetchCollection = function(url, next){
   }, false);
   request.open('GET', url, true);
   request.send();
+};
+
+
+
+// make a generic loading document
+var makeLoadingDocument = function(){
+  var str = [
+    '<document>',
+      '<loadingTemplate>',
+        '<activityIndicator>',
+          '<title>Loading...</title>',
+        '</activityIndicator>',
+      '</loadingTemplate>',
+    '</document>',
+  ].join('');
+
+  var parser  = new DOMParser();
+  return parser.parseFromString(str, 'text/xml');
+};
+
+
+
+var scheduleSlideRemoval = function(nextSlide){
+  setTimeout(function(){
+    // remove the event handler so it doesn't get called for the wrong slide
+    slides.lastPlayed.removeEventListener('unload', backButtonHandler);
+
+    // pressing the back button can sometimes mess us all up
+    // just deal with it and don't do anything about it
+    try{
+      navigationDocument.removeDocument(slides.lastPlayed);
+    } catch(e){}
+
+    //
+    slides.lastPlayed = nextSlide;
+  }, 1000);
 };
 
 
